1a2016e7ccfafafdcaf278cab47b8f53b81122c0
marcoespudario-sys <marcoespudario@estudante.ufla.br>
2026-09-10 11:26:32 -0300

    Cria o jogo Star Walker

---
+++ code/script.js
@@ -0,0 +1,170 @@
+class Player {
+  constructor(game) {
+    this.game = game;
+    this.width = 100;
+    this.height = 100;
+    this.x = this.game.width * 0.5 - this.width * 0.5;
+    this.y = this.game.height - this.height;
+    this.speed = 5;
+  }
+  draw(context) {
+    context.fillRect(this.x, this.y, this.width, this.height);
+  }
+  update() {
+    // movimento do player na horizontal
+    if (this.game.keys.indexOf("ArrowLeft") > -1) this.x -= this.speed;
+    if (this.game.keys.indexOf("ArrowRight") > -1) this.x += this.speed;
+    // limite do movimento
+    if (this.x < -this.width * 0.5) this.x = -this.width * 0.5;
+    else if (this.x > this.game.width - this.width * 0.5)
+      this.x = this.game.width - this.width * 0.5;
+  }
+  shoot(){
+    const projectile = this.game.getProjectile();
+    if (projectile) projectile.start(this.x + this.width * 0.5, this.y);
+  }
+}
+
+class Projectile {
+  constructor() {
+    this.width = 4;
+    this.height = 20;
+    this.x = 0;
+    this.y = 0;
+    this.speed = 20;
+    this.free = true;
+  }
+  draw(context) {
+    if (!this.free) {
+      context.fillRect(this.x, this.y, this.width, this.height);
+    }
+  }
+  update() {
+    if (!this.free) {
+      this.y -= this.speed;
+      if(this.y < 0 -this.height) this.reset();
+    }
+  }
+  start(x, y) {
+    this.x = x - this.width * 0.5;
+    this.y = y;
+    this.free = false;
+  }
+  reset() {
+    this.free = true;
+  }
+}
+
+class Enemy {
+    constructor(game){
+        this.game = game;
+        this.width;
+        this.height;
+        this.x;
+        this.y;
+    }
+    draw(context){
+        context.strokeRect(this.x, this.y, this.width, this.height);
+    }
+    update(){
+
+    }
+}
+
+class Wave{
+    constructor(game){
+        this.game = game;
+        this.width = this.game.columns * this.game.enemySize;
+        this.height = this.game.rows * this.game.enemySize;
+        this.x = 0;
+        this.y = 0;
+        this.speedX = 3;
+        this.speedY = 0;
+    }
+
+    render(context){
+        this.speedY = 0;
+        context.strokeRect(this.x, this.y, this.width, this.height);
+        if (this.x < 0 || this.x > this.game.width - this.width){
+            this.speedX *= -1;
+            this.speedY = this.game.enemySize;
+        }
+        this.x += this.speedX;
+        this.y += this.speedY;
+    }
+}
+
+class Game {
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.width = this.canvas.width;
+    this.height = this.canvas.height;
+    this.player = new Player(this);
+    this.keys = [];
+
+    this.projectilePool = [];
+    this.numberOfProjectiles = 10;
+    this.createProjectiles();
+
+    this.columns = 5;
+    this.rows = 8;
+    this.enemySize = 60;
+
+    this.waves = [];
+    this.waves.push(new Wave(this));
+
+    window.addEventListener("keydown", (e) => {
+      if (this.keys.indexOf(e.key) === -1) this.keys.push(e.key);
+      if (e.key === '1') this.player.shoot();
+    });
+    window.addEventListener("keyup", (e) => {
+      const index = this.keys.indexOf(e.key);
+      if (index > -1) this.keys.splice(index, 1);
+    });
+  }
+  render(context) {
+    this.player.draw(context);
+    this.player.update();
+    this.projectilePool.forEach(projectile => {
+        projectile.update();
+        projectile.draw(context);
+    })
+    this.waves.forEach(wave => {
+        wave.render(context);
+    })
+
+  }
+
+  createProjectiles(){
+    for(let i = 0; i < this.numberOfProjectiles; i++){
+        this.projectilePool.push(new Projectile());
+    }
+  }
+
+  getProjectile(){
+    for (let i = 0; i < this.projectilePool.length; i++) {
+        if (this.projectilePool[i].free) return this.projectilePool[i];
+    }
+  }
+}
+
+// para que o jogo só rode quando toda a pagina for carregada ("load")
+window.addEventListener("load", function () {
+  const canvas = document.getElementById("canvas1");
+  const ctx = canvas.getContext("2d");
+  canvas.width = 600;
+  canvas.height = 800;
+  ctx.fillStyle = "white";
+  ctx.strokeStyle = "white";
+  ctx.lineWidth = 5;
+
+  const game = new Game(canvas);
+
+  function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    game.render(ctx);
+    requestAnimationFrame(animate);
+  }
+
+  animate();
+});
